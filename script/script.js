b4c23d7dec9b61ab8ee32670cb3a03acd0884a1f
michaelsusanto81 <michaelsusanto32@gmail.com>
2019-07-03 17:38:18 +0700

    Added computer effect

--- script/script.js
+++ script/script.js
@@ -16,22 +16,41 @@ function battle(player, comp) {
 	}
 }
 
+function generateComp() {
+	let i = 0;
+	const start = new Date().getTime();
+	setInterval(function() {
+		if(new Date().getTime() - start > 1000) {
+			clearInterval;
+			return;
+		}
+		comp.src = images[i++];
+		if(i == images.length) i = 0;
+	}, 40)
+}
+
 const container = document.querySelector('.container');
 const liveScore = document.getElementById('liveScore');
 const info = container.querySelector('.info');
 const comp = container.querySelector('.comp');
+const images = ['img/rock.png', 'img/paper.png', 'img/scissors.png'];
 let playerScore = 0, compScore = 0;
 
 container.addEventListener('click', function(e) {
 	if(e.target.className == 'player') {
 		const p = e.target.src.split(/[/.]/)[8];
 		const c = compChoice();
-		info.innerHTML = battle(p, c);
+		generateComp();
 
-		if(info.innerHTML == 'You Win') playerScore++;
-		if(info.innerHTML == 'You Lose') compScore++;
+		setTimeout(function() {
+			info.innerHTML = battle(p, c);
 
-		comp.src = 'img/' + c + '.png';
-		liveScore.innerHTML = playerScore + ' : ' + compScore;
+			if(info.innerHTML == 'You Win') playerScore++;
+			if(info.innerHTML == 'You Lose') compScore++;
+
+			comp.src = 'img/' + c + '.png';
+			liveScore.innerHTML = playerScore + ' : ' + compScore;
+		}, 1000);
+		
 	}
 });
